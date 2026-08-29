a6fdfb6e619dd0214d64928a7b84a3f70d3b4dc0
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-08-29 21:14:56 +0530

    Added the needed command to work with Redis

---
+++ utils/cache.js
@@ -0,0 +1,28 @@
+const { redisClient } = require("../config/redis")
+
+// get command
+const getCache = async (key)=>{
+    const data = await redisClient.get(key);
+    if(!data){
+        return null;
+    }
+    return JSON.parse(data); // converting the string data that we get from redis cloud
+}
+
+const setCache = async(key, data, ttl=300)=>{
+    await redisClient.setEx(
+        key,
+        ttl,
+        JSON.stringify(data) // convdrting josn data to string to store redis cloud, since redis will accept only key-value pairs.
+    )
+}
+
+const deleteCache = async(key)=>{
+    await redisClient.del(key);
+}
+
+module.exports= {
+    getCache,
+    setCache,
+    deleteCache
+}
