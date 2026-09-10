197568981cd6bbc8b643741aaaf01c13e6b7677d
poojarijayaprakash4 <pjayaprakash072@gmail.com>
2026-09-10 17:13:44 +0530

    Added the Customer and Address Schemas

---
+++ models/Customer/Address.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+
+const addressSchema = new mongoose.Schema(
+    {
+        label:{
+            type:String,
+            enum:["Home","Work","Other"],
+            default:"Home"
+        },
+        fullName:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        phone:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        addressLine1:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        addressLine2:{
+            type:String,
+            default:"",
+            trim:true
+        },
+        city:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        state:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        pincode:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        latitude:{
+            type:Number,
+            required:true
+        },
+        longitude:{
+            type:Number,
+            required:true
+        },
+        isDefault:{
+            type:Boolean,
+            default:false
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+module.exports = mongoose.model("Address",addressSchema);
---
+++ models/Customer/Customer.js
@@ -0,0 +1,58 @@
+const mongoose = require('mongoose')
+const customerSchema = new mongoose.Schema(
+    {
+        name:{
+            type:String,
+            required:true,
+            trim:true
+        },
+        email:{
+            type:String,
+            required:true,
+            trim:true,
+            unique:true,
+            lowercase:true
+        },
+        phone:{
+            type:String,
+            required: function(){
+                return this.authProvider === "local"
+            }
+        },
+        password:{
+            type:String,
+            required:function(){
+                return this.authProvider === "local"
+            }
+        },
+        profileImg:{
+            url:{
+                type:String,
+                default:""
+            },
+            public_id:{
+                type:String,
+                default:""
+            }
+        },
+        authProvider:{
+            type:String,
+            enum:["local","google"],
+            default:"local"
+        },
+        googleId:{
+            type:String,
+            unique:true,
+            sparse:true
+        },
+        isVerified:{
+            type:Boolean,
+            default:false
+        }
+    },
+    {
+        timestamps:true
+    }
+)
+
+module.exports = mongoose.model("Customer",customerSchema);
